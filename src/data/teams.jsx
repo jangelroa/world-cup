706026bbb106fcc8f58090e23976ed17215054a5
Oslo Lisboa <oslolisboa@Oslos-MacBook-Pro.local>
2022-10-10 02:37:05 -0700

    Added group B

--- src/data/teams.jsx
+++ src/data/teams.jsx
@@ -1,10 +1,94 @@
-const teams = [
+import netherlands from "../images/flags/netherlands.webp";
+import senegal from "../images/flags/senegal.webp";
+import ecuador from "../images/flags/ecuador.webp";
+import qatar from "../images/flags/qatar.webp";
+
+// import england from "../images/svg/qa.svg";
+import england from "../images/flags/england.webp";
+import usa from "../images/flags/usa.webp";
+import wales from "../images/flags/wales.webp";
+import iran from "../images/flags/iran.webp";
+
+import spain from "../images/svg/es.svg";
+
+export const groupA = [
   {
-    id: 12,
-    name: "Spain",
-    flag: "/assets/flags/spain",
+    fifa: 8,
+    name: "Netherlands",
+    flag: netherlands,
     points: 0,
+    group: "A",
+    flagAlt: "Netherlands flag",
+    // flagAlt: "${this.name} flag",
+  },
+  {
+    fifa: 18,
+    name: "Senegal",
+    flag: senegal,
+    points: 0,
+    group: "A",
+    flagAlt: "Senegal flag",
+  },
+  {
+    fifa: 44,
+    name: "Ecuador",
+    flag: ecuador,
+    points: 0,
+    group: "A",
+    flagAlt: "Ecuador flag",
+  },
+  {
+    fifa: 50,
+    name: "Qatar",
+    flag: qatar,
+    points: 0,
+    group: "A",
+    flagAlt: "Qatar flag",
   },
 ];
 
-export default teams;
+export const groupB = [
+  {
+    fifa: 5,
+    name: "England",
+    flag: england,
+    points: 0,
+    group: "B",
+    flagAlt: "England flag",
+  },
+  {
+    fifa: 16,
+    name: "USA",
+    flag: usa,
+    points: 0,
+    group: "B",
+    flagAlt: "USA flag",
+  },
+  {
+    fifa: 19,
+    name: "Wales",
+    flag: wales,
+    points: 0,
+    group: "B",
+    flagAlt: "Wales flag",
+  },
+  {
+    fifa: 20,
+    name: "Iran",
+    flag: iran,
+    points: 0,
+    group: "B",
+    flagAlt: "Iran flag",
+  },
+];
+
+export const groupE = [
+  {
+    fifa: 7,
+    name: "Spain",
+    flag: spain,
+    points: 0,
+    group: "E",
+    flagAlt: "Spain flag",
+  },
+];
