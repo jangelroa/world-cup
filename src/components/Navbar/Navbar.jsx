9752afc6d85bda2234ac807b92b4599a216d0545
Oslo Lisboa <oslolisboa@Oslos-MacBook-Pro.local>
2022-10-17 02:12:58 -0700

    Added Mobile menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Nav,
   NavLink,
@@ -6,40 +6,112 @@ import {
   NavMenu,
   NavBtn,
   NavBtnLink,
+  NavDropDownList,
 } from "./NavbarElements";
+import { ThickLine, ThinLine } from "../smallComponents";
 import logo from "../../images/qatar-logo.jpg";
 import { Link } from "react-router-dom";
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      const dimentions = getWindowDimensions();
+      setWindowDimensions(dimentions);
+      if (dimentions.width > 768) {
+        setShowMobileMenu(false);
+      }
+      console.log({ dimentions });
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
       <Nav>
-        {/* <NavMenu> */}
         <NavLink to="/">
           <img src={logo} alt="logo" style={{ height: "80px" }} />
         </NavLink>
-        <NavLink to="/matches">GAMES</NavLink>
-        <NavLink to="/team-scores" style={{ fontWeight: "bold" }}>
+        <Bars onClick={() => setShowMobileMenu((show) => !show)} />
+        <NavMenu>
           <div>
-            <div style={{ textAlign: "center" }}>TEAM</div>
-            <div style={{ textAlign: "center" }}>SCORES</div>
+            <NavLink to="/matches">GAMES</NavLink>
           </div>
-        </NavLink>
-        <NavLink to="/user-scores" style={{ fontWeight: "bold" }}>
           <div>
-            <div style={{ textAlign: "center" }}>USER</div>
-            <div style={{ textAlign: "center" }}>SCORES</div>
+            <NavLink to="/team-scores" style={{ fontWeight: "bold" }}>
+              <div>
+                <div style={{ textAlign: "center" }}>TEAM</div>
+                <div style={{ textAlign: "center" }}>SCORES</div>
+              </div>
+            </NavLink>
           </div>
-        </NavLink>
-        <NavLink to="/add-pool">ADD POOL</NavLink>
-        {/* </NavMenu> */}
+          <div>
+            <NavLink to="/user-scores" style={{ fontWeight: "bold" }}>
+              <div>
+                <div style={{ textAlign: "center" }}>PARTICIPANTS</div>
+                <div style={{ textAlign: "center" }}>SCORES</div>
+              </div>
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to="/add-pool">ADD POOL</NavLink>
+          </div>
+        </NavMenu>
       </Nav>
+      <NavDropDown
+        showMobileMenu={showMobileMenu}
+        setShowMobileMenu={setShowMobileMenu}
+      />
     </>
   );
 };
 
 export default Navbar;
 
+const NavDropDown = ({ showMobileMenu, setShowMobileMenu }) =>
+  showMobileMenu && (
+    <div
+      className="nav-drop-down-list"
+      onClick={() => setShowMobileMenu(false)}
+    >
+      <ThinLine />
+      <ul>
+        <li>
+          <NavLink to="/">HOME</NavLink>
+        </li>
+        <li>
+          <NavLink to="/matches">GAMES</NavLink>
+        </li>
+        <li>
+          <NavLink to="/team-scores" style={{ fontWeight: "bold" }}>
+            TEAM SCORES
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/user-scores" style={{ fontWeight: "bold" }}>
+            PARTICIPANTS SCORES
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/add-pool">ADD POOL</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 {
   /* 
 <Nav>
