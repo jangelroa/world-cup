08894240eeddeab1aded50f7466d7bd1ab9b7d1a
Oslo Lisboa <oslolisboa@Oslos-MacBook-Pro.local>
2022-11-23 11:44:48 -0800

    Filtered 'delete' users. Added more game scores and highlights

--- src/data/highLights.jsx
+++ src/data/highLights.jsx
@@ -39,6 +39,114 @@ const highLights = {
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     ></iframe>
   ),
+  "Argentina-Saudi Arabia": (
+    <iframe
+      width="100%"
+      height="100%"
+      src="https://www.youtube.com/embed/H6zHbVQjeaw"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    ></iframe>
+  ),
+  "Denmark-Tunisia": (
+    <iframe
+      width="100%"
+      height="100%"
+      src="https://www.youtube.com/embed/ppaZH3uURqk"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    ></iframe>
+  ),
+  "Mexico-Poland": (
+    <iframe
+      width="100%"
+      height="100%"
+      src="https://www.youtube.com/embed/xO2iHpGuPbg"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    ></iframe>
+  ),
+  "France-Australia": (
+    <iframe
+      width="100%"
+      height="100%"
+      src="https://www.youtube.com/embed/D0kVR0s7p7g"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    ></iframe>
+  ),
+  "Morocco-Croatia": (
+    <iframe
+      width="100%"
+      height="100%"
+      src="https://www.youtube-nocookie.com/embed/ZAQRRU79vEE"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    ></iframe>
+  ),
+  "Germany-Japan": (
+    <iframe
+      width="100%"
+      height="100%"
+      src="https://www.youtube-nocookie.com/embed/PvZoSogBCzs"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    ></iframe>
+  ),
+  "Spain-Costa Rica": (
+    <iframe
+      width="100%"
+      height="100%"
+      src="https://www.youtube-nocookie.com/embed/2hSVDLPWeKA"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    ></iframe>
+  ),
+  "Belgium-Canada": null,
+  "Switzerland-Cameroon": null,
+  "Uruguay-South Korea": null,
+  "Portugal-Ghana": null,
+  "Brazil-Serbia": null,
+  "Wales-Iran": null,
+  "Qatar-Senegal": null,
+  "Netherlands-Ecuador": null,
+  "England-USA": null,
+  "Tunisia-Australia": null,
+  "Poland-Saudi Arabia": null,
+  "France-Denmark": null,
+  "Argentina-Mexico": null,
+  "Japan-Costa Rica": null,
+  "Belgium-Morocco": null,
+  "Croatia-Canada": null,
+  "Spain-Germany": null,
+  "Cameroon-Serbia": null,
+  "South Korea-Ghana": null,
+  "Brazil-Switzerland": null,
+  "Portugal-Uruguay": null,
+  "Ecuador-Senegal": null,
+  "Netherlands-Qatar": null,
+  "Iran-USA": null,
+  "Wales-England": null,
+  "Australia-Denmark": null,
+  "Tunisia-France": null,
+  "Poland-Argentina": null,
+  "Saudi Arabia-Mexico": null,
+  "Canada-Morocco": null,
+  "Croatia-Belgium": null,
+  "Costa Rica-Germany": null,
+  "Japan-Spain": null,
+  "Ghana-Uruguay": null,
+  "South Korea-Portugal": null,
+  "Cameroon-Brazil": null,
+  "Serbia-Switzerland": null,
+  "": null,
   "Semi-Final 1 Winner-Semi-Final 2 Winner": (
     <iframe
       width="100%"
